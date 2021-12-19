19912f2879b5ef63474f506cb6e9e5a4892007c3
hoangleduy27 <hlduy.19it5@vku.udn.vn>
2021-12-19 20:49:04 +0700

    make it better

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 const socket = io("/");
+
 const chatInputBox = document.getElementById("chat_message");
 const all_messages = document.getElementById("all_messages");
 const main__chat__window = document.getElementById("main__chat__window");
