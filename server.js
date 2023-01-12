891377d520ae784202575c57319a0b8a1ab89554
hoangleduy27 <70272512+hoangleduy27@users.noreply.github.com>
2023-01-12 15:45:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -32,21 +32,29 @@ io.on("connection", (socket) => {
         });
     });
 });
-const arrUserInfo = [];
-io.on('connection', socket => {
-      socket.on('nguoi_dung_dang_ky', user => {
-        const isExist = arrUserInfo.some(e => e.username === user.username);
-        socket.id = user.id;
+const arrUserInfo = []
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected", userId);
+
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message);
+        });
+    });
+    socket.on('NGUOI_DUNG_DANG_KY', user => {
+        const isExist = arrUserInfo.some(e => e.ten === user.ten);
+        socket.peerId = user.peerId;
         if (isExist) return socket.emit('DANG_KY_THAT_BAT');
         arrUserInfo.push(user);
         socket.emit('DANH_SACH_ONLINE', arrUserInfo);
         socket.broadcast.emit('CO_NGUOI_DUNG_MOI', user);
-        console.log(username)
     });
+
     socket.on('disconnect', () => {
-        const index = arrUserInfo.findIndex(user => user.id === socket.id);
+        const index = arrUserInfo.findIndex(user => user.peerId === socket.peerId);
         arrUserInfo.splice(index, 1);
-        io.emit('AI_DO_NGAT_KET_NOI', socket.id);
+        io.emit('AI_DO_NGAT_KET_NOI', socket.peerId);
     });
 });
 server.listen(process.env.PORT || 3030);
