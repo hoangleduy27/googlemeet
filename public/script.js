79d386492e468d3c237cb2fe4928870e5a16ecfa
hoangleduy27 <70272512+hoangleduy27@users.noreply.github.com>
2023-01-12 16:28:40 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -19,13 +19,13 @@ socket.on('DANH_SACH_ONLINE', arrUserInfo => {
 
     arrUserInfo.forEach(user => {
         const { ten, peerId } = user;
-        uluser.append('<li>${ten}</li>');
+        uluser.append("<li>${ten}</li>");
     });
 
-    socket.on('CO_NGUOI_DUNG_MOI', user => {
-        const { ten, peerId } = user;
-        uluser.append('<li>${ten}</li>');
-    });
+//     socket.on('CO_NGUOI_DUNG_MOI', user => {
+//         const { ten, peerId } = user;
+//         uluser.append("<li>${ten}</li>");
+//     });
 
     socket.on('AI_DO_NGAT_KET_NOI', peerId => {
         user.remove();
